3abe3c580dc0f726f6fffab21891f646e92b4a5d
michaelcchu <50280000+michaelcchu@users.noreply.github.com>
2023-01-07 20:52:22 -0500

    extract some basic information from midi file

--- index.js
+++ index.js
@@ -2,7 +2,30 @@ input.addEventListener("change", () => {
     for (const file of input.files) {
         const reader = new FileReader();
         reader.addEventListener("load", (e) => {
-            console.log(e.target.result);
+            const view = new Uint8Array(e.target.result);
+            const header = {
+                chunkId: view.slice(0,4),
+                chunkSize: view.slice(4,8),
+                formatType: view.slice(8,10),
+                numberOfTracks: view.slice(10,12),
+                timeDivision: view.slice(12,14)
+            };
+            const trackChunks = [];
+            let i = 14;
+            while (i < view.length) {
+                const trackChunk = {
+                    chunkID: view.slice(i,i+4),
+                    chunkSize: view.slice(i+4,i+8)
+                }
+                i+=8
+                trackChunks.push(trackChunk);
+            }
+
+            const midi = {header:header, trackChunks:trackChunks};
+
+            console.log(view);
+            console.log(midi);
+
         });
         reader.readAsArrayBuffer(file);
     }
