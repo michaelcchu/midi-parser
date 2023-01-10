5f36b0def5d813a0a86d267c7aa1caddba846ec7
michaelcchu <50280000+michaelcchu@users.noreply.github.com>
2023-01-09 22:10:39 -0500

    added read function to simplify parsing

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-function toInt(array) {
-    let result = 0;
-    for (let i = 0; i < array.length; i++) {
-        result += array[i] * (2**8)**(array.length-1-i);
+function str(value, i, times) {return String.fromCharCode(value);}
+function int(value, i, times) {return value * (2**8)**(times-1-i);}
+
+function read(f, startValue, values, times) {
+    let result = startValue;
+    for (let i = 0; i < times; i++) {
+        result += f(values.next().value, i, times);
     }
     return result;
+}
+
+function readVarLen(values) {
+    const num = []; let byte; 
+    do {byte = values.next().value;
+        if (byte >= 128) {num.push(byte - 128);} else {num.push(byte);}
+    } while (byte >= 128);
+    return read(int, 0, num.values(), num.length);
 }
 
 input.addEventListener("change", () => {
@@ -11,34 +22,48 @@ input.addEventListener("change", () => {
         const reader = new FileReader();
         reader.addEventListener("load", (e) => {
             const view = new Uint8Array(e.target.result);
+            const values = view.values();
             const header = {
-                chunkId: view.slice(0,4),
-                chunkSize: view.slice(4,8),
-                formatType: view.slice(8,10),
-                numberOfTracks: view.slice(10,12),
-                timeDivision: view.slice(12,14)
+                chunkId: read(str, "", values, 4), 
+                chunkSize: read(int, 0, values, 4), 
+                formatType: read(int, 0, values, 2),
+                numberOfTracks: read(int, 0, values, 2), 
+                timeDivision: read(int, 0, values, 2)
             };
+
             const trackChunks = [];
-            let i = 14;
-            while (i < view.length) {
+            for (let track = 0; track < header.numberOfTracks; track++) {
                 const trackChunk = {
-                    chunkID: view.slice(i,i+4),
-                    chunkSize: view.slice(i+4,i+8),
+                    chunkID: read(str, "", values, 4), 
+                    chunkSize: read(int, 0, values, 4),
                     trackEventData: []
                 }
-                i+=8
-                const chunkEnd = i + toInt(trackChunk.chunkSize);
-                while (i < chunkEnd) {
-                    start = i; while (view[i] >= 128) {i++;} i++;
+                while (true) {
                     const trackEvent = {
-                        deltaTime: view.slice(start,i),
-                        eventType: view.slice(i,i+1),
-                        midiChannel: view.slice(i,i+1),
-                        parameter1: view.slice(i+1,i+2),
-                        parameter2: view.slice(i+2,i+3)
+                        deltaTime: readVarLen(values),
+                        eventType: read(int, 0, values, 1)
+                    }
+                    if ((trackEvent.eventType >= 80) && 
+                        (trackEvent.eventType < 240)) {
+                        trackEvent.parameter1 = read(int, 0, values, 1);
+                        trackEvent.parameter2 = read(int, 0, values, 1);
+                    } else if (eventType === 255) {
+                        // meta event
+                        trackEvent.metaEventType = read(int, 0, values, 1);
+                        if (trackEvent.metaEventType === 0) {
+                            trackEvent.msb = read(int, 0, values, 1);
+                            trackEvent.lsb = read(int, 0, values, 1);
+                        } else if (trackEvent.metaEventType === 1) {
+
+                        }
+
+                        // write response here
+                    } else {
+                        console.log("error");
+                        // write response here
                     }
                     trackChunk.trackEventData.push(trackEvent);
-                    i+=3;
+                    break;
                 }
                 trackChunks.push(trackChunk);
             }
