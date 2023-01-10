225d738fa299c62f2d0ef80de2f6f8a9950fe3fc
michaelcchu <50280000+michaelcchu@users.noreply.github.com>
2023-01-09 23:41:47 -0500

    added code for parsing meta events

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ function read(f, startValue, values, times) {
     return result;
 }
 
-function readVarLen(values) {
+function readVarLen(f, startValue, values) {
     const num = []; let byte; 
     do {byte = values.next().value;
         if (byte >= 128) {num.push(byte - 128);} else {num.push(byte);}
     } while (byte >= 128);
-    return read(int, 0, num.values(), num.length);
+    return read(f, startValue, num.values(), num.length);
 }
 
 input.addEventListener("change", () => {
@@ -34,35 +34,56 @@ input.addEventListener("change", () => {
             const trackChunks = [];
             for (let track = 0; track < header.numberOfTracks; track++) {
                 const trackChunk = {
-                    chunkID: read(str, "", values, 4), 
+                    chunkId: read(str, "", values, 4), 
                     chunkSize: read(int, 0, values, 4),
                     trackEventData: []
                 }
                 while (true) {
-                    const trackEvent = {
-                        deltaTime: readVarLen(values),
-                        eventType: read(int, 0, values, 1)
+                    const e = {
+                        deltaTime: readVarLen(int, 0, values),
+                        eventType: read(int, 0, values, 1),
                     }
-                    if ((trackEvent.eventType >= 80) && 
-                        (trackEvent.eventType < 240)) {
-                        trackEvent.parameter1 = read(int, 0, values, 1);
-                        trackEvent.parameter2 = read(int, 0, values, 1);
-                    } else if (eventType === 255) {
-                        // meta event
-                        trackEvent.metaEventType = read(int, 0, values, 1);
-                        if (trackEvent.metaEventType === 0) {
-                            trackEvent.msb = read(int, 0, values, 1);
-                            trackEvent.lsb = read(int, 0, values, 1);
-                        } else if (trackEvent.metaEventType === 1) {
-
+                    if ((e.eventType >= 80) && (e.eventType < 240)) {
+                        e.parameter1 = read(int, 0, values, 1);
+                        e.parameter2 = read(int, 0, values, 1);
+                    } else if (e.eventType === 255) {
+                        e.metaType = read(int, 0, values, 1);
+                        e.length = readVarLen(int, 0, values);
+                        if (e.metaType === 0) {
+                            e.msb = read(int, 0, values, 1);
+                            e.lsb = read(int, 0, values, 1);
+                        } else if (e.metaType >= 1 && e.metaType <= 7) {
+                            e.text = readVarLen(str, "", values);
+                        } else if (e.metaType === 32) {
+                            e.channel = read(int, 0, values, 1);
+                        } else if (e.metaType === 47) {
+                            console.log("meta event: end of track");
+                        } else if (e.metaType === 81) {
+                            e.microsecondsPerQuarter = read(int, 0, values, 3);
+                        } else if (e.metaType === 84) {
+                            e.hours = read(int, 0, values, 1);
+                            e.minutes = read(int, 0, values, 1);
+                            e.seconds = read(int, 0, values, 1);
+                            e.frames = read(int, 0, values, 1);
+                            e.subframes = read(int, 0, values, 1);
+                        } else if (e.metaType === 88) {
+                            e.numerator = read(int, 0, values, 1);
+                            e.denominator = read(int, 0, values, 1);
+                            e.metronomePulse = read(int, 0, values, 1);
+                            e.numberOf32ndsPerQuarter = read(int, 0, values, 1);
+                        } else if (e.metaType === 89) {
+                            e.key = read(int, 0, values, 1);
+                            e.scale = read(int, 0, values, 1);
+                        } else if (e.metaType === 127) {
+                            e.data = readVarLen(str, "", values);
+                        } else {
+                            console.log("unknown metaType: " + e.metaType);
                         }
-
-                        // write response here
                     } else {
-                        console.log("error");
+                        console.log("unknown eventType: " + e.eventType);
                         // write response here
                     }
-                    trackChunk.trackEventData.push(trackEvent);
+                    trackChunk.trackEventData.push(e);
                     break;
                 }
                 trackChunks.push(trackChunk);
